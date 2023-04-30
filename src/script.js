9b3d4247c5bc1add3283c97cc4e55e38ce0dd5a7
Fehér Gábor <gabor.feher.25@gmail.com>
2023-04-30 23:03:37 +0200

    Base of game

--- src/script.js
+++ src/script.js
@@ -1,3 +1,12 @@
-$(document).ready({
+let game_area;
+let player;
+$(document).ready(function(){
 
-})
+    game_area = $('#game_area');
+    player= $('<img id="player" src="../assets/player.png" alt="null"/> ');
+
+    player.css({
+        top: 500 + 'px'
+    })
+    game_area.append(player);
+});
