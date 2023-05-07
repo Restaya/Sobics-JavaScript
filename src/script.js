7343162c32cfd6df77a718d7eeb1bee3216141a3
Fehér Gábor <h982734@stud.u-szeged.hu>
2023-05-07 16:06:56 +0200

    Added High score list and animation sound

--- src/script.js
+++ src/script.js
@@ -8,6 +8,7 @@ let app_width;
 
 let time_bar;
 
+let body;
 // Player, it's width and height
 let player;
 let player_width;
@@ -24,9 +25,11 @@ let block_height;
 // block in player's hand
 let current_block = null;
 
-let score = 0;
-let score_tab;
+let score = 10;
+let score_div;
 
+let error_div;
+let error_message;
 //time
 let start;
 let max_time;
@@ -34,25 +37,42 @@ let timeoutVal = Math.floor(max_time/100);
 
 // How many times the player threw a block
 let number_of_throws = 0;
+let throws_div;
+
+let high_scores;
+let high_scores_div;
 
 let colors = ["#207EE3","#ED6C09","#F0E21F","#990913","#E80E9F"];
 
+let pop_sound;
+let whoosh_sound;
+
+// variables for the form;
+let name_form;
+let username;
+let save_score_button;
 
 $(document).ready(function(){
 
     time_bar = $('#time_bar');
     game_area = $('#game_area');
     app = $('#app');
+    body = $("body");
 
     player= $('<img id="player" src="../assets/player.png" alt="null"/> ');
     game_area.append(player);
 
-    score_tab = '<div id="score_tab">Score: <span id="score"></span></div>'
-
-    score_tab = '<div id="score_tab">Score: <span id="score"></span></div>'
-
-    $("body").append(score_tab)
+    score_div = $('<div id="score_div">Score: <span id="score"></span></div>');
+    body.append(score_div)
     $('#score').append(score)
+
+    error_div = $('<div id="error_div"><span id="message"></span></div>')
+    body.append(error_div);
+    $('#message').append(error_message);
+
+    throws_div = $('<div id="throws_div">Thrown blocks: <span id="throws"></span></div>');
+    body.append(throws_div)
+    $('#throws').append(number_of_throws)
 
     app_height = app.height();
     app_width = app.width();
@@ -62,6 +82,12 @@ $(document).ready(function(){
 
     block_width = game_area_width/10;
     block_height = game_area_height/12;
+
+    pop_sound = document.createElement('audio');
+    pop_sound.setAttribute('src','../assets/pop_sound.wav');
+
+    whoosh_sound = document.createElement('audio');
+    whoosh_sound.setAttribute('src','../assets/whoosh_sound.wav')
 
     player.on('load', function () {
         init_player();
@@ -157,13 +183,17 @@ function generate_blocks(){
 // Player actions, get a block or throws if has one already
 function player_action(){
 
+    if(number_of_throws === 5){
+        game_end();
+    }
+
     // Throws a block
     if(has_block === true){
 
         let i = 7;
         while(true){
             if (blocks[6][get_player_column()] !== null){
-                console.log("No more space to throw!")
+                $('#message').text("No more space to throw!");
                 return;
             }
             // Checks for last non-null block in column
@@ -176,10 +206,13 @@ function player_action(){
                 has_block = false;
 
                 number_of_throws++;
+                $('#throws').text(number_of_throws);
 
+                pop_sound.play();
+
+                $('#message').text("");
                 update_score(number_of_throws);
                 change_max_time();
-                console.log(max_time)
                 return;
             }
             i--;
@@ -193,19 +226,20 @@ function player_action(){
         while(true){
 
             if ( i < 0){
-                console.log("No blocks in this column!");
+                $('#message').text("No blocks in this column!");
                 break;
             }
             if (blocks[i][get_player_column()] !== null){
-                console.log(i);
                 current_block = blocks[i][get_player_column()];
-                //console.log("Got a block at " + (i) + " row!");
                 blocks[i][get_player_column()] = null;
                 has_block = true;
 
+                whoosh_sound.play();
                 current_block.animate({
                     top: game_area_height - player_height - block_height  + 'px'
                 },500)
+
+                $('#message').text("");
 
                 break;
             }
@@ -257,7 +291,89 @@ function change_max_time(){
     }
 }
 
+function game_end(){
+    app.remove();
+    throws_div.remove();
+    $('#music').remove();
 
+
+    score_div.css({
+        top: 25+ '%',
+        left: 40 + '%'
+    })
+    name_form =  $('<div id="name_form">' +
+        '<form>' +
+        '<input type="text" name="username" id="username" placeholder="Enter your name!"/>' + '<br>' + '<br>' +
+        '<button type="submit" class="button" id="save_score_button" onclick="save_score(event)">Save score!</button>' +
+        '</form>' +
+        '</div>')
+
+    body.append(name_form);
+}
 function check_blocks(block){
     //TODO
+}
+
+// Saves the score and lists the high scores when the button is clicked
+function save_score(e){
+    e.preventDefault();
+
+    username = document.getElementById("username");
+    save_score_button = document.getElementById("save_score_button");
+
+    if(!username.value){
+        $('#message').text("You have to enter your name!");
+        return;
+    }
+
+    let name_score={
+        name: username.value,
+        high_score:score
+    };
+
+    high_scores = JSON.parse(localStorage.getItem('high_scores')) || [];
+
+    high_scores.push(name_score);
+
+    high_scores.sort(function (a,b) {return b.high_score - a.high_score;} )
+
+    high_scores.splice(3)
+
+    localStorage.setItem('high_scores',JSON.stringify(high_scores));
+
+    save_score_button.disabled = true;
+
+    list_high_scores();
+
+}
+
+// Lists the 3 top high score
+function list_high_scores(){
+
+    high_scores = JSON.parse(localStorage.getItem('high_scores')) || [];
+
+    let second_row,third_row;
+
+    if(high_scores[1] != null){
+        second_row = high_scores[1].name + ": " + high_scores[1].high_score;
+    }else{
+        second_row="No second place yet";
+    }
+    if(high_scores[2] != null){
+        third_row = high_scores[2].name + ": " + high_scores[2].high_score;
+    }else{
+        second_row="No third place yet";
+    }
+
+
+    high_scores_div = $('<div id="high_scores_div">' +
+                        '<ol id="high_scores_list">' +
+                        '<li>' + high_scores[0].name + ": " + high_scores[0].high_score + ' points</li>' +
+                        '<li>' + second_row + ' points</li>' +
+                        '<li>' + third_row + ' points</li>' +
+                        '</ol>' +
+                        '</div>');
+
+    body.append(high_scores_div);
+
 }
